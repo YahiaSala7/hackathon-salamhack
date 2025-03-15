5422beff3574a7e6abbceabb50fa3590be7271a4
fotgoodlife1 <fotgoodlife78@gmail.com>
2025-03-15 21:48:39 +0200

    fix

---
+++ SalamHack.Data/DTOS/User/UserDto.cs
@@ -0,0 +1,13 @@
+﻿/*namespace SalamHack.DTOs
+{
+    // DTO للمستخدم
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
+
+
+*/
