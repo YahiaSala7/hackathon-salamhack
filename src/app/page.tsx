5b5a2175cbbbac6394d2102d193cd8d83d033e93
mustafa haider <mustafahaider351@gmail.com>
2025-03-16 20:27:59 +0300

    added: hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/Components/Home/Header";
+import HeroSection from "@/Components/Home/HeroSection";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <HeroSection />
     </>
   );
 }
