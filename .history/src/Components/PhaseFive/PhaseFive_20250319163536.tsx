d9128eba83b180e0ed366eecd9d98f2c5f076de6
mustafa haider <mustafahaider351@gmail.com>
2025-03-20 22:37:03 +0300

    no history pls

--- .history/src/Components/PhaseFive/PhaseFive_20250319163536.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function PhaseFive() {
-  return <div>PhaseFive</div>;
-}
-
-export default PhaseFive;
--- .history/src/hooks/usePlanningData_20250320161718.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-
-export const usePlanningData = () => {
-  return useQuery({
-    queryKey: ["planningData"],
-    staleTime: Infinity, // Cache forever unless manually invalidated
-    enabled: false, // Prevents automatic refetching
-  });
-};
